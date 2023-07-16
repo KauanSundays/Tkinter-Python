5f0f888f1578804de356ec23e2e9b0675cf9ac41
Kauan Sundays <102335585+KauanSundays@users.noreply.github.com>
2023-07-16 08:52:03 -0300

    feat: "title"

--- AppWidgets.py
+++ AppWidgets.py
@@ -2,3 +2,11 @@ import tkinter as tk
 
 # Create a Window for app
 window = tk.Tk()
+
+
+window.title('Window and widgets') #titulo
+window.geometry('800x500') #largura do app padrao
+
+
+# run the app
+window.mainloop()
