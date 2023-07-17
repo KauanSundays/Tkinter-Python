dfd506f24ec73905fe5ee805a5cacdb5adafb66c
Kauan Sundays <102335585+KauanSundays@users.noreply.github.com>
2023-07-17 19:28:40 -0300

    feat: "teste teste teste"

--- AppWidgets.py
+++ AppWidgets.py
@@ -11,6 +11,6 @@ window.geometry('800x500') #largura do app padrao
 text = tk.Text(master = window)#textbox
 text.pack() #conteudo em variavel
 
-
+test teste teste de github
 # run the app
 window.mainloop()
