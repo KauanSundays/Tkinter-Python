91f2f4947698ee2987dee60499b329270cbf7b8f
Kauan Sundays <102335585+KauanSundays@users.noreply.github.com>
2023-07-19 23:16:03 -0300

    feat: "title of app"

--- AppWidgets.py
+++ AppWidgets.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import ttk
 
 # Create a Window for app
 window = tk.Tk()
@@ -7,10 +8,14 @@ window = tk.Tk()
 window.title('Window and widgets') #titulo
 window.geometry('800x500') #largura do app padrao
 
+#label, title
+label = ttk.Label(master = window, text= 'This is a test')
+label.pack()
+
 #Create the widgets
 text = tk.Text(master = window)#textbox
 text.pack() #conteudo em variavel
 
-test teste teste de github
+
 # run the app
 window.mainloop()
